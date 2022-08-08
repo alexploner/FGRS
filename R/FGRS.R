3905d5f2cae879cdda36ae861c7daee95608e7c3
Alexander Ploner <alexander.ploner@ki.se>
2022-08-08 09:00:53 +0200

    Added documentation for example

---
+++ R/FGRS.R
@@ -0,0 +1,13 @@
+#' Example data for FGRS calculation
+#'
+#' A small synthetic data set that tracks the same diagnosis in both
+#' probands and relatives
+#'
+#' @name ex1
+#' @docType data
+#' @format A list with two data frames, `probands` and `relatives`
+#' @details
+#'     FIXME
+#'
+"ex1"
+
